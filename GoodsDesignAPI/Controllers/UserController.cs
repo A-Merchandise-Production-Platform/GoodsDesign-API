ca0387d2eb0f09c72850ef3fc49a5af2855ce850
Hoang Tien <143521414+hoangtien1105@users.noreply.github.com>
2025-01-25 12:13:41 +0700

    fix errors

--- GoodsDesignAPI/Controllers/UserController.cs
+++ GoodsDesignAPI/Controllers/UserController.cs
@@ -8,7 +8,7 @@ using System.Security.Claims;
 
 namespace GoodsDesignAPI.Controllers
 {
-    [Route("api/user")]
+    [Route("api")]
     [ApiController]
     public class UserController : ControllerBase
     {
