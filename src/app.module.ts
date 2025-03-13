a80941efac70ce463995e368737d5d0bc0961115
uydev <lequocuyit@gmail.com>
2025-03-13 11:38:44 +0700

    feat: change price fields from Decimal to Int in various models and update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,12 @@ import { EnvModule } from "./dynamic-modules"
 import { CategoriesModule } from "./categories"
 import { ProductsModule } from "./products"
 import { RedisModule } from "./redis"
-import { BlankVariancesModule } from './blank-variances/blank-variances.module';
 import { SystemConfigModule } from "./system-config"
 import { GraphQLModule } from "@nestjs/graphql"
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
 import { join } from "path"
 import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default"
+import { BlankVariancesModule } from "./blank-variances/blank-variances.module"
 // import { TestModule } from './test/test.module';
 
 @Module({
@@ -32,7 +32,8 @@ import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin
         CategoriesModule,
         ProductsModule,
         RedisModule,
-        SystemConfigModule
+        SystemConfigModule,
+        BlankVariancesModule,
         // TestModule,
     ],
     controllers: [AppController],
