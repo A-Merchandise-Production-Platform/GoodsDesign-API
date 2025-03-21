b623d55c5d530a857e7f8e1ad80553c7141f70a8
uydev <lequocuyit@gmail.com>
2025-03-21 18:09:06 +0700

    fix: Update package dependencies and remove obsolete migration files
    - Downgraded @nestjs-modules/mailer from version 2.0.2 to 1.6.1 in package.json and package-lock.json.
    - Removed several obsolete migration SQL files related to previous database schema changes, including user and product design tables.
    - Cleaned up unnecessary entries in the package-lock.json to reflect the current state of dependencies.

--- prisma/migrations/20250316071940_add_analyst_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Users" ADD COLUMN     "certifications" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "specialization" TEXT,
-ADD COLUMN     "yearsOfExperience" INTEGER;
---
+++ prisma/migrations/20250321110614_init/migration.sql
@@ -0,0 +1,502 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('ADMIN', 'MANAGER', 'STAFF', 'FACTORYOWNER', 'CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'ACCEPTED', 'IN_PRODUCTION', 'COMPLETED', 'DELIVERED', 'CANCELED');
+
+-- CreateEnum
+CREATE TYPE "QualityCheckStatus" AS ENUM ('PENDING', 'PARTIAL_APPROVED', 'APPROVED', 'REJECTED');
+
+-- CreateEnum
+CREATE TYPE "ReworkStatus" AS ENUM ('NOT_REQUIRED', 'IN_PROGRESS', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "PaymentType" AS ENUM ('DEPOSIT', 'WITHDRAWN');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('PAYMENT', 'REFUND');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('VNPAY', 'PAYOS');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('COMPLETED', 'PENDING', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "FactoryStatus" AS ENUM ('PENDING_APPROVAL', 'APPROVED', 'REJECTED', 'SUSPENDED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "phoneNumber" TEXT,
+    "password" TEXT NOT NULL DEFAULT '',
+    "gender" BOOLEAN NOT NULL DEFAULT false,
+    "dateOfBirth" TIMESTAMP(3),
+    "imageUrl" TEXT DEFAULT '',
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT,
+    "updatedAt" TIMESTAMP(3),
+    "updatedBy" TEXT,
+    "deletedBy" TEXT,
+    "deletedAt" TIMESTAMP(3),
+    "role" "Roles" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Addresses" (
+    "id" TEXT NOT NULL,
+    "provinceID" INTEGER NOT NULL,
+    "districtID" INTEGER NOT NULL,
+    "wardCode" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT,
+    "updatedAt" TIMESTAMP(3),
+    "updatedBy" TEXT,
+    "deletedAt" TIMESTAMP(3),
+    "deletedBy" TEXT,
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "model3DUrl" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdBy" TEXT,
+    "updatedAt" TIMESTAMP(3),
+    "updatedBy" TEXT,
+    "deletedAt" TIMESTAMP(3),
+    "deletedBy" TEXT,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SystemConfigBank" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "bin" TEXT NOT NULL,
+    "shortName" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "SystemConfigBank_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SystemConfigColor" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "SystemConfigColor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SystemConfigSize" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "SystemConfigSize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlankVariances" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "information" JSONB NOT NULL,
+    "blankPrice" INTEGER NOT NULL,
+
+    CONSTRAINT "BlankVariances_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductPositionType" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "positionName" TEXT NOT NULL,
+    "basePrice" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductPositionType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductDesign" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "blankVariantId" TEXT NOT NULL,
+    "saved3DPreviewUrl" TEXT NOT NULL,
+    "isFinalized" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "isTemplate" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "ProductDesign_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DesignPosition" (
+    "id" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "productPositionTypeId" TEXT NOT NULL,
+    "designJSON" JSONB NOT NULL,
+
+    CONSTRAINT "DesignPosition_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FavoriteDesign" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FavoriteDesign_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CustomerOrder" (
+    "id" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "status" "OrderStatus" NOT NULL,
+    "totalPrice" INTEGER NOT NULL,
+    "shippingPrice" INTEGER NOT NULL,
+    "depositPaid" INTEGER NOT NULL,
+    "orderDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CustomerOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CustomerOrderDetail" (
+    "id" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "status" "OrderStatus" NOT NULL,
+    "qualityCheckStatus" "QualityCheckStatus" NOT NULL,
+    "reworkStatus" "ReworkStatus" NOT NULL,
+
+    CONSTRAINT "CustomerOrderDetail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "type" "PaymentType" NOT NULL,
+    "paymentLog" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "status" "PaymentStatus" NOT NULL,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PaymentTransaction" (
+    "id" TEXT NOT NULL,
+    "paymentId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "paymentGatewayTransactionId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "paymentMethod" "PaymentMethod" NOT NULL,
+    "status" "TransactionStatus" NOT NULL,
+    "transactionLog" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PaymentTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderHistory" (
+    "id" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "note" TEXT,
+
+    CONSTRAINT "OrderHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "taskname" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "expiredTime" TIMESTAMP(3) NOT NULL,
+    "qualityCheckStatus" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CheckQuality" (
+    "id" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "orderDetailId" TEXT NOT NULL,
+    "totalChecked" INTEGER NOT NULL,
+    "passedQuantity" INTEGER NOT NULL,
+    "failedQuantity" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "reworkRequired" BOOLEAN NOT NULL,
+    "note" TEXT,
+    "checkedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CheckQuality_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StaffTask" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "assignedDate" TIMESTAMP(3) NOT NULL,
+    "note" TEXT,
+    "status" TEXT NOT NULL,
+    "completedDate" TIMESTAMP(3),
+
+    CONSTRAINT "StaffTask_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FactoryProduct" (
+    "id" TEXT NOT NULL,
+    "factoryId" TEXT NOT NULL,
+    "blankVarianceId" TEXT NOT NULL,
+    "productionCapacity" INTEGER NOT NULL,
+    "estimatedProductionTime" INTEGER NOT NULL,
+
+    CONSTRAINT "FactoryProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Factory" (
+    "factoryOwnerId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "businessLicenseUrl" TEXT,
+    "taxIdentificationNumber" TEXT,
+    "address" TEXT,
+    "website" TEXT,
+    "yearEstablished" INTEGER,
+    "totalEmployees" INTEGER,
+    "maxPrintingCapacity" INTEGER,
+    "qualityCertifications" TEXT,
+    "primaryPrintingMethods" TEXT,
+    "specializations" TEXT,
+    "contactPersonName" TEXT,
+    "contactPersonRole" TEXT,
+    "contactPhone" TEXT,
+    "operationalHours" TEXT,
+    "leadTime" INTEGER,
+    "minimumOrderQuantity" INTEGER,
+    "factoryStatus" "FactoryStatus" NOT NULL DEFAULT 'PENDING_APPROVAL',
+    "statusNote" TEXT,
+    "contractAccepted" BOOLEAN NOT NULL DEFAULT false,
+    "contractAcceptedAt" TIMESTAMP(3),
+    "reviewedBy" TEXT,
+    "reviewedAt" TIMESTAMP(3),
+    "contract" JSONB,
+
+    CONSTRAINT "Factory_pkey" PRIMARY KEY ("factoryOwnerId")
+);
+
+-- CreateTable
+CREATE TABLE "FactoryOrder" (
+    "id" TEXT NOT NULL,
+    "factoryId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "estimatedCompletionDate" TIMESTAMP(3) NOT NULL,
+    "totalItems" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "totalProductionCost" INTEGER NOT NULL,
+
+    CONSTRAINT "FactoryOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FactoryOrderDetail" (
+    "id" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "factoryOrderId" TEXT NOT NULL,
+    "orderDetailId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "productionCost" INTEGER NOT NULL,
+
+    CONSTRAINT "FactoryOrderDetail_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "title" TEXT,
+    "content" TEXT,
+    "url" TEXT,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CartItems" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "designId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CartItems_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Addresses" ADD CONSTRAINT "Addresses_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Products" ADD CONSTRAINT "Products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BlankVariances" ADD CONSTRAINT "BlankVariances_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductPositionType" ADD CONSTRAINT "ProductPositionType_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductDesign" ADD CONSTRAINT "ProductDesign_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductDesign" ADD CONSTRAINT "ProductDesign_blankVariantId_fkey" FOREIGN KEY ("blankVariantId") REFERENCES "BlankVariances"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DesignPosition" ADD CONSTRAINT "DesignPosition_designId_fkey" FOREIGN KEY ("designId") REFERENCES "ProductDesign"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DesignPosition" ADD CONSTRAINT "DesignPosition_productPositionTypeId_fkey" FOREIGN KEY ("productPositionTypeId") REFERENCES "ProductPositionType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FavoriteDesign" ADD CONSTRAINT "FavoriteDesign_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FavoriteDesign" ADD CONSTRAINT "FavoriteDesign_designId_fkey" FOREIGN KEY ("designId") REFERENCES "ProductDesign"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomerOrder" ADD CONSTRAINT "CustomerOrder_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomerOrderDetail" ADD CONSTRAINT "CustomerOrderDetail_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "CustomerOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomerOrderDetail" ADD CONSTRAINT "CustomerOrderDetail_designId_fkey" FOREIGN KEY ("designId") REFERENCES "ProductDesign"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "CustomerOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentTransaction" ADD CONSTRAINT "PaymentTransaction_paymentId_fkey" FOREIGN KEY ("paymentId") REFERENCES "Payment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentTransaction" ADD CONSTRAINT "PaymentTransaction_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "CustomerOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentTransaction" ADD CONSTRAINT "PaymentTransaction_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderHistory" ADD CONSTRAINT "OrderHistory_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "CustomerOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CheckQuality" ADD CONSTRAINT "CheckQuality_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CheckQuality" ADD CONSTRAINT "CheckQuality_orderDetailId_fkey" FOREIGN KEY ("orderDetailId") REFERENCES "CustomerOrderDetail"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StaffTask" ADD CONSTRAINT "StaffTask_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StaffTask" ADD CONSTRAINT "StaffTask_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryProduct" ADD CONSTRAINT "FactoryProduct_factoryId_fkey" FOREIGN KEY ("factoryId") REFERENCES "Factory"("factoryOwnerId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryProduct" ADD CONSTRAINT "FactoryProduct_blankVarianceId_fkey" FOREIGN KEY ("blankVarianceId") REFERENCES "BlankVariances"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Factory" ADD CONSTRAINT "Factory_factoryOwnerId_fkey" FOREIGN KEY ("factoryOwnerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryOrder" ADD CONSTRAINT "FactoryOrder_factoryId_fkey" FOREIGN KEY ("factoryId") REFERENCES "Factory"("factoryOwnerId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryOrderDetail" ADD CONSTRAINT "FactoryOrderDetail_factoryOrderId_fkey" FOREIGN KEY ("factoryOrderId") REFERENCES "FactoryOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryOrderDetail" ADD CONSTRAINT "FactoryOrderDetail_designId_fkey" FOREIGN KEY ("designId") REFERENCES "ProductDesign"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FactoryOrderDetail" ADD CONSTRAINT "FactoryOrderDetail_orderDetailId_fkey" FOREIGN KEY ("orderDetailId") REFERENCES "CustomerOrderDetail"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItems" ADD CONSTRAINT "CartItems_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItems" ADD CONSTRAINT "CartItems_designId_fkey" FOREIGN KEY ("designId") REFERENCES "ProductDesign"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
