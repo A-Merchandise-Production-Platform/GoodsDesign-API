aaa274d11397edfecb2dbde42bc130f91ff76128
uydev <lequocuyit@gmail.com>
2025-03-24 07:51:39 +0700

    refactor: Remove unused ConfigModule import from payment gateway module

--- src/payment-gateway/payment-gateway.module.ts
+++ src/payment-gateway/payment-gateway.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PaymentGatewayService } from './payment-gateway.service';
-import { ConfigModule } from '@nestjs/config';
 import { PaymentGatewayResolver } from './payment-gateway.resolver';
 
 @Module({
