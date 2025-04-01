32f72f5c69076f0b2a82bc91cf6c2c4d90c97f67
uydev <lequocuyit@gmail.com>
2025-04-01 13:25:31 +0700

    feat: Update CORS configuration to allow requests from additional domain
    - Modified the CORS settings in `main.ts` to include "https://goodsdesign.uydev.id.vn" as an allowed origin alongside "http://localhost:3000", enhancing the application's accessibility for different environments.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ import { graphqlUploadExpress } from 'graphql-upload-ts'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors({
-        origin: ["http://localhost:3000"],
+        origin: [
+            "http://localhost:3000",
+            "https://goodsdesign.uydev.id.vn"
+        ],
         credentials: true
     })
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, }))
