62dbd6ae8aab5f903a0d1e771e483aec0f23d314
hnam-truong <truonghahaonam@gmail.com>
2025-04-02 21:24:01 +0700

    feat: Update thumbnail URLs in product designs seed data

--- prisma/seeds/data/product-designs.data.ts
+++ prisma/seeds/data/product-designs.data.ts
@@ -13,33 +13,36 @@ interface ProductDesignsData {
 }
 
 export const productDesignsData: ProductDesignsData = {
-  productDesigns: [
-    {
-      id: "design001",
-      userEmail: "customer@gmail.com",
-      systemConfigVariantId: "var001",
-      isFinalized: true,
-      isPublic: true,
-      isTemplate: true,
-      thumbnailUrl: "https://example.com/thumbnails/design001.jpg"
-    },
-    {
-      id: "design002",
-      userEmail: "customer@gmail.com",
-      systemConfigVariantId: "var002",
-      isFinalized: true,
-      isPublic: false,
-      isTemplate: false,
-      thumbnailUrl: "https://example.com/thumbnails/design002.jpg"
-    },
-    {
-      id: "design003",
-      userEmail: "manager@gmail.com",
-      systemConfigVariantId: "var003",
-      isFinalized: false,
-      isPublic: false,
-      isTemplate: false,
-      thumbnailUrl: "https://example.com/thumbnails/design003.jpg"
-    },
-  ],
-};
+    productDesigns: [
+        {
+            id: "design001",
+            userEmail: "customer@gmail.com",
+            systemConfigVariantId: "var001",
+            isFinalized: true,
+            isPublic: true,
+            isTemplate: true,
+            thumbnailUrl:
+                "https://files.cdn.printful.com/o/upload/product-catalog-img/ea/ea968722fc4980abb0a61b90e7cc0451_l"
+        },
+        {
+            id: "design002",
+            userEmail: "customer@gmail.com",
+            systemConfigVariantId: "var002",
+            isFinalized: true,
+            isPublic: false,
+            isTemplate: false,
+            thumbnailUrl:
+                "hhttps://files.cdn.printful.com/o/upload/product-catalog-img/ea/ea968722fc4980abb0a61b90e7cc0451_l"
+        },
+        {
+            id: "design003",
+            userEmail: "manager@gmail.com",
+            systemConfigVariantId: "var003",
+            isFinalized: false,
+            isPublic: false,
+            isTemplate: false,
+            thumbnailUrl:
+                "https://files.cdn.printful.com/o/upload/product-catalog-img/ea/ea968722fc4980abb0a61b90e7cc0451_l"
+        }
+    ]
+}
