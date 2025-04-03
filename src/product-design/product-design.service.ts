5fb0bbc0991d8bf512e5014c6c8a17ef8f9603d0
hnam-truong <truonghahaonam@gmail.com>
2025-04-03 17:29:50 +0700

    feat: Update categories and improve product design service methods

--- src/product-design/product-design.service.ts
+++ src/product-design/product-design.service.ts
@@ -1,150 +1,157 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { CreateProductDesignDto } from './dto/create-product-design.dto';
-import { UpdateProductDesignDto } from './dto/update-product-design.dto';
-import { ProductDesignEntity } from './entities/product-design.entity';
+import { Injectable } from "@nestjs/common"
+import { PrismaService } from "../prisma/prisma.service"
+import { CreateProductDesignDto } from "./dto/create-product-design.dto"
+import { UpdateProductDesignDto } from "./dto/update-product-design.dto"
+import { ProductDesignEntity } from "./entities/product-design.entity"
 
 @Injectable()
 export class ProductDesignService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async create(createProductDesignDto: CreateProductDesignDto): Promise<ProductDesignEntity> {
-    const { userId, systemConfigVariantId, ...designData } = createProductDesignDto;
+    async create(createProductDesignDto: CreateProductDesignDto): Promise<ProductDesignEntity> {
+        const { userId, systemConfigVariantId, ...designData } = createProductDesignDto
 
-    if (!systemConfigVariantId) {
-      throw new Error('systemConfigVariantId is required');
+        if (!systemConfigVariantId) {
+            throw new Error("systemConfigVariantId is required")
+        }
+
+        const systemConfigVariant = await this.prisma.systemConfigVariant.findFirst({
+            where: {
+                id: systemConfigVariantId
+            }
+        })
+
+        //get position type
+        const positionType = await this.prisma.productPositionType.findMany({
+            where: {
+                productId: systemConfigVariant.productId
+            }
+        })
+
+        const designPositionsData = positionType.map((p) => {
+            return {
+                designJSON: null,
+                productPositionTypeId: p.id
+            }
+        })
+
+        const data = await this.prisma.productDesign.create({
+            data: {
+                ...designData,
+                user: {
+                    connect: { id: userId }
+                },
+                systemConfigVariant: {
+                    connect: { id: systemConfigVariantId }
+                },
+                designPositions: {
+                    createMany: {
+                        data: designPositionsData
+                    }
+                }
+            },
+            include: {
+                user: true,
+                systemConfigVariant: {
+                    include: {
+                        product: true
+                    }
+                },
+                designPositions: {
+                    include: {
+                        positionType: true
+                    }
+                }
+            }
+        })
+        return new ProductDesignEntity(data)
     }
 
-    const systemConfigVariant = await this.prisma.systemConfigVariant.findFirst({
-      where: {
-        id: systemConfigVariantId
-      }
-    })
+    async findAll(userId?: string): Promise<ProductDesignEntity[]> {
+        const data = await this.prisma.productDesign.findMany({
+            where: userId ? { userId } : undefined,
+            include: {
+                user: true,
+                systemConfigVariant: {
+                    include: {
+                        product: {
+                            include: {
+                                category: true
+                            }
+                        }
+                    }
+                },
+                designPositions: {
+                    include: {
+                        positionType: true
+                    }
+                }
+            }
+        })
+        return data.map((item) => new ProductDesignEntity(item))
+    }
 
-    //get position type
-    const positionType = await this.prisma.productPositionType.findMany({
-      where: {
-        productId: systemConfigVariant.productId
-      }
-    })
+    async findOne(id: string): Promise<ProductDesignEntity> {
+        const data = await this.prisma.productDesign.findUnique({
+            where: { id },
+            include: {
+                user: true,
+                systemConfigVariant: {
+                    include: {
+                        product: true
+                    }
+                },
+                designPositions: {
+                    include: {
+                        positionType: true
+                    }
+                }
+            }
+        })
+        return new ProductDesignEntity(data)
+    }
 
-    const designPositionsData = positionType.map(p => {
-      return {
-        designJSON: null,
-        productPositionTypeId: p.id
-      }
-    })
-    
-    const data = await this.prisma.productDesign.create({
-      data: {
-        ...designData,
-        user: {
-          connect: { id: userId }
-        },
-        systemConfigVariant: {
-          connect: { id: systemConfigVariantId }
-        },
-        designPositions: {
-          createMany: {
-            data: designPositionsData
-          }
-        }
-      },
-      include: {
-        user: true,
-        systemConfigVariant: {
-          include: {
-            product: true,
-          },
-        },
-        designPositions: {
-          include: {
-            positionType: true,
-          },
-        },
-      },
-    });
-    return new ProductDesignEntity(data);
-  }
+    async update(
+        id: string,
+        updateProductDesignDto: UpdateProductDesignDto
+    ): Promise<ProductDesignEntity> {
+        const data = await this.prisma.productDesign.update({
+            where: { id },
+            data: updateProductDesignDto,
+            include: {
+                user: true,
+                systemConfigVariant: {
+                    include: {
+                        product: true
+                    }
+                },
+                designPositions: {
+                    include: {
+                        positionType: true
+                    }
+                }
+            }
+        })
+        return new ProductDesignEntity(data)
+    }
 
-  async findAll(userId?: string): Promise<ProductDesignEntity[]> {
-    const data = await this.prisma.productDesign.findMany({
-      where: userId ? { userId } : undefined,
-      include: {
-        user: true,
-        systemConfigVariant: {
-          include: {
-            product: true,
-          },
-        },
-        designPositions: {
-          include: {
-            positionType: true,
-          },
-        },
-      },
-    });
-    return data.map(item => new ProductDesignEntity(item));
-  }
-
-  async findOne(id: string): Promise<ProductDesignEntity> {
-    const data = await this.prisma.productDesign.findUnique({
-      where: { id },
-      include: {
-        user: true,
-        systemConfigVariant: {
-          include: {
-            product: true,
-          },
-        },
-        designPositions: {
-          include: {
-            positionType: true,
-          },
-        },
-      },
-    });
-    return new ProductDesignEntity(data);
-  }
-
-  async update(id: string, updateProductDesignDto: UpdateProductDesignDto): Promise<ProductDesignEntity> {
-    const data = await this.prisma.productDesign.update({
-      where: { id },
-      data: updateProductDesignDto,
-      include: {
-        user: true,
-        systemConfigVariant: {
-          include: {
-            product: true,
-          },
-        },
-        designPositions: {
-          include: {
-            positionType: true,
-          },
-        },
-      },
-    });
-    return new ProductDesignEntity(data);
-  }
-
-  async remove(id: string): Promise<ProductDesignEntity> {
-    const data = await this.prisma.productDesign.delete({
-      where: { id },
-      include: {
-        user: true,
-        systemConfigVariant: {
-          include: {
-            product: true,
-          },
-        },
-        designPositions: {
-          include: {
-            positionType: true,
-          },
-        },
-      },
-    });
-    return new ProductDesignEntity(data);
-  }
-} 
+    async remove(id: string): Promise<ProductDesignEntity> {
+        const data = await this.prisma.productDesign.delete({
+            where: { id },
+            include: {
+                user: true,
+                systemConfigVariant: {
+                    include: {
+                        product: true
+                    }
+                },
+                designPositions: {
+                    include: {
+                        positionType: true
+                    }
+                }
+            }
+        })
+        return new ProductDesignEntity(data)
+    }
+}
