7bff1d1cd750117f7e002138b0533c2219e112d3
Ngo Gia Huan <ngogiahuan23122003@gmail.com>
2025-04-05 09:13:15 +0700

    refactor: Simplify factory products data structure
    - Removed unnecessary id fields from factory products data.
    - Streamlined the mapping of variants to factory products for better readability and maintainability.
    - Ensured consistent formatting and indentation in the data structure.

--- prisma/seeds/data/factory-products.data.ts
+++ prisma/seeds/data/factory-products.data.ts
@@ -1,25 +1,21 @@
-import { factoriesData } from './factories.data';
-import { variantsData } from './system-config-variants.data';
-
-
+import { factoriesData } from "./factories.data"
+import { variantsData } from "./system-config-variants.data"
 
 export const factoryProductsData = [
-  ...variantsData.variants.map((v, index) => {
-    return {
-      id: 'factoryproduct00'+index,
-      factoryId: factoriesData[0].factoryOwnerId,
-      systemConfigVariantId: v.id,
-      productionCapacity: 1000,
-      estimatedProductionTime: 7, // days
-    }
-  }),
-  ...variantsData.variants.map((v, index) => {
-    return {
-      id: 'factoryproduct-11'+index,
-      factoryId: factoriesData[1].factoryOwnerId,
-      systemConfigVariantId: v.id,
-      productionCapacity: 500,
-      estimatedProductionTime: 4, // days
-    }
-  }),
-]; 
+    ...variantsData.variants.map((v, index) => {
+        return {
+            factoryId: factoriesData[0].factoryOwnerId,
+            systemConfigVariantId: v.id,
+            productionCapacity: 1000,
+            estimatedProductionTime: 7 // days
+        }
+    }),
+    ...variantsData.variants.map((v, index) => {
+        return {
+            factoryId: factoriesData[1].factoryOwnerId,
+            systemConfigVariantId: v.id,
+            productionCapacity: 500,
+            estimatedProductionTime: 4 // days
+        }
+    })
+]
