eeda3026c811425ee68bb6ba3cbb61612bcca4fa
uydev <lequocuyit@gmail.com>
2025-05-07 17:59:44 +0700

    refactor: simplify PrismaService by removing transaction options and connection management
    - Removed transaction options and the OnModuleInit interface from PrismaService, streamlining the service for direct database interactions without additional configuration.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,8 @@
 // src/prisma/prisma.service.ts
 
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      transactionOptions: {
-        maxWait: 10000, // Maximum time to wait for transaction to start (10 seconds)
-        timeout: 15000, // Maximum time for transaction to complete (15 seconds)
-      },
-    });
-  }
-
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
+export class PrismaService extends PrismaClient {}
 
